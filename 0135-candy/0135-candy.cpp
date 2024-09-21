9b3c455e4014166d3fa6712b44db6349765dbae1
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-09-22 02:12:35 +0530

    Time: 22 ms (11.38%), Space: 20.3 MB (69.37%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int candy(vector<int>& r) {
+        int n=r.size();
+        vector<int> can(n,1); // every kid has min 1 candy
+
+        for(int i=1;i<n;i++){
+            if(r[i]>r[i-1]){
+                can[i]=can[i-1]+1;
+            }
+        }
+
+        for(int i=n-2;i>=0;i--){
+            if(r[i]>r[i+1] && can[i]<can[i+1]+1){
+                can[i]=can[i+1]+1;
+            }
+        }
+
+        int s=0;
+        for(auto i:can){
+            s+=i;
+        }
+
+        return s;
+
+    }
+};
