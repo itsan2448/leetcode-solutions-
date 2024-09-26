e9dea6ee167a30255052f27bde5241ebaf4666e9
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-09-26 15:43:52 +0530

    Time: 15 ms (81.13%), Space: 13.8 MB (61.99%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int calculate(string s) {
+        stack<int> st;
+        char op = '+';
+        int a=0;
+        for(int i=0;s[i];i++){
+            if(isdigit(s[i])){
+                a=a*10+(s[i]-'0');
+            }
+            if(!isdigit(s[i]) && s[i]!=' ' || i==s.size()-1){
+                if(op=='+') st.push(a);
+                else if(op=='-') st.push(-a);
+                else if(op=='*'){
+                    int tmp=st.top();
+                    st.pop();
+                    st.push(tmp*a);
+                }
+                else if(op=='/'){
+                    int tmp=st.top();
+                    st.pop();
+                    st.push(tmp/a);
+                }
+                op=s[i];
+                a=0;
+            }            
+        }
+        a=0;
+        while(!st.empty()){
+            a+=st.top();st.pop();
+        }
+        return a;
+    }
+};
