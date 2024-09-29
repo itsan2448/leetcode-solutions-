a63096ae8a43fd14ecc37dfdc86af0b5beb39793
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-09-29 19:37:12 +0530

    Time: 365 ms (10.97%), Space: 161.7 MB (14.81%) - LeetHub

--- 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.cpp
+++ 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int mx=0,ans=0;
-    void dfs(int h, unordered_map<int,vector<int>> &adj, vector<bool> &vis,vector<int>& i){
+    void dfs(int h, unordered_map<int,vector<int>> &adj,vector<int>& i){
         mx=max(mx,ans);
-        if(!vis[h]){
-            vis[h]=1;
+        // if(!vis[h]){
+            // vis[h]=1;
             for(auto j:adj[h]){
                 ans+=i[h];
-                dfs(j,adj,vis,i);
+                dfs(j,adj,i);
                 ans-=i[h];
             }
-        }
+        // }
     }
     int numOfMinutes(int n, int h, vector<int>& m, vector<int>& i) {
         if(n==1) return 0;
@@ -19,8 +19,8 @@ public:
             if(m[i]!=-1)
             adj[m[i]].push_back(i);
         }
-        vector<bool> vis(n,0);
-        dfs(h,adj,vis,i);
+        // vector<bool> vis(n,0);
+        dfs(h,adj,i);
         return mx;
     }
 };
