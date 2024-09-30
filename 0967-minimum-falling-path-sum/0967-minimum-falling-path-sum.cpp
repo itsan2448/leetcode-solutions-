2d7116746f5c824167001b17e51171126fb33ff1
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-09-30 15:28:58 +0530

    Time: 11 ms (70.76%), Space: 13.3 MB (87.27%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -7,15 +7,15 @@ public:
         
         for(int i=r-2;i>=0;i--){
             for(int j=0;j<c;j++){
-                int l = (j>0)?m[i+1][j-1]:INT_MAX;
-                int r = (j<c-1)?m[i+1][j+1]:INT_MAX;
-                int d = m[i+1][j];
+                int l = (j>0)?m[i+1][j-1]:INT_MAX; // down left diag
+                int r = (j<c-1)?m[i+1][j+1]:INT_MAX; //Down right diag
+                int d = m[i+1][j]; //below
                 m[i][j] += min(d,min(l,r));
             }
         }
         int ans=INT_MAX;
         
-        for (int j = 0; j < c; ++j) {
+        for (int j=0; j<c; ++j) {
             ans = min(ans, m[0][j]);
         }
         return ans;
