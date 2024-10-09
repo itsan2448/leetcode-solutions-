27f5490f0828c7d435c8be1e7776ff190adedb7d
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-10-09 15:54:30 +0530

    Time: 39 ms (26.49%), Space: 16.5 MB (30.64%) - LeetHub

---
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        o,c=0,0
+        for i in range(len(s)):
+            if s[i]=='(':
+                o+=1
+            elif s[i]==')' and o>0:
+                o-=1
+            else :
+                c+=1                      
+
+        return o+c
+        
+        
