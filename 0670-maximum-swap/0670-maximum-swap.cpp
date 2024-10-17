6a1da376fe5afa9c0fd12a09d9ce07d485d6aefc
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-10-17 12:43:06 +0530

    Time: 0 ms (100%), Space: 7.7 MB (31.77%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.cpp
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -3,18 +3,18 @@ public:
     int maximumSwap(int num) {
         string s=to_string(num);
         int n=s.size();
-        if(n<=1) return num;        
-        
-        int ans=num;
+        vector<int> occ(10,-1);
         for(int i=0;i<n;i++){
-            for(int j=n-1;j>i;j--){
-                if(s[i]<s[j]){
-                    swap(s[i],s[j]);
-                    ans=max(ans,stoi(s));
-                    swap(s[i],s[j]);
+            occ[s[i]-'0']=i;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=9;j>s[i]-'0';j--){
+                if(occ[j]>i){
+                    swap(s[i],s[occ[j]]);
+                    return stoi(s);
                 }
             }
         }
-        return ans;
+        return num;
     }
 };
