54b8b7b77bcd740feb09498fcd868bc295c107b2
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-10-19 12:41:40 +0530

    Time: 56 ms (23.04%), Space: 83.8 MB (24.58%) - LeetHub

---
+++ 1667-find-kth-bit-in-nth-binary-string/1667-find-kth-bit-in-nth-binary-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string inv_rev(string s){
+        if(s=="") return s;
+        reverse(s.begin(),s.end());
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='1') s[i]='0';
+            else s[i]='1';
+        }
+        return s;
+    }
+    char findKthBit(int n, int k) {
+        string s="0";
+        // string s1="0";
+        for(int i=1;i<n;i++){
+            string tmp=s+'1'+inv_rev(s);
+            s=tmp;
+        }
+        return s[k-1];
+    }
+};
