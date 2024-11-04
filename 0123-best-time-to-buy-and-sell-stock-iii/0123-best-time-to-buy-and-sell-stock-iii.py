5514f8ebba9cdc5ca6fd9e38fd01fa28795944ac
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-11-04 21:06:12 +0530

    Time: 1118 ms (22.71%), Space: 59.8 MB (21.82%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,4 +1,5 @@
 class Solution:
+    '''
     def f(self,buy, cap, ind, p,n,dp):
         # memoisation
         if cap==0 or ind==n: 
@@ -11,15 +12,45 @@ class Solution:
         else:
             dp[ind][buy][cap]= max(p[ind]+self.f(1,cap-1,ind+1,p,n,dp),self.f(0,cap,ind+1,p,n,dp))
         return dp[ind][buy][cap]
-                
+    '''
+
     def maxProfit(self, p: List[int]) -> int:
         
         n=len(p)
+        dp=[[[0]*3 for i in range(2)] for j in range(n+1)]
+        
+        '''
+        # base case 1 : cap=0
+        for i in range(n-1,-1,-1):
+            for j in range(2):                
+                dp[i][j][0] = 0
+        # base case 2 : 
+        for j in range(2):
+            for k in range(3):
+                dp[n][j][k]=0
+        '''
+
+        for i in range(n-1,-1,-1):
+            for j in range(2):
+                for k in range(1,3): #cap=0, base case
+                    if j==1:
+                        dp[i][j][k]= max(-p[i]+dp[i+1][0][k],dp[i+1][1][k])
+                    else :
+                        dp[i][j][k]=max(p[i]+dp[i+1][1][k-1],dp[i+1][0][k])
+
+        return dp[0][1][2]
+
+
+
+        '''
         # memoisation
         # TC = O(n x cap x 2)
         # Space = O(n x cap x 2) + auxillary stack space
         dp=[[[-1]*3 for i in range(2)] for j in range(n)]
         return self.f(1,2,0,p,n,dp)
+        '''
+
+
 
         
 
