b3f389a7df6446ba9afae1fed9b6b4a3b62d706b
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-11-04 16:18:24 +0530

    Time: 3 ms (37.23%), Space: 17.6 MB (92.47%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -2,20 +2,14 @@ class Solution:
     
     def maxProfit(self, p: List[int]) -> int:
         n = len(p)
-                
-        dp=[[0]*2 for _ in range(n+1)]
-        
-        # base case
-        dp[n][0]=dp[n][1]=0
+         
+        aheadBuy, aheadNotBuy = 0,0
 
-        for i in range (n-1,-1,-1):                            
-            for j in range(2):
-                profit =0
-                if j:
-                    profit = max(-p[i]+dp[i+1][0],dp[i+1][1])
-                else :
-                    profit = max(p[i]+dp[i+1][1],dp[i+1][0])
-                dp[i][j] = profit
+        for i in range (n-1,-1,-1): 
+            curNotBuy = max(p[i]+aheadBuy,aheadNotBuy)
+            curBuy = max(-p[i]+aheadNotBuy,aheadBuy)
             
-        # j=1 coz, we should be in the position to buy 
-        return dp[0][1]
+            aheadBuy, aheadNotBuy=curBuy, curNotBuy 
+            
+
+        return aheadBuy
