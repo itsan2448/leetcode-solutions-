6817cb6f4bc2fa635f67549d6d3eb433aaf36900
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-11-05 12:26:17 +0530

    Time: 3 ms (77.72%), Space: 17.3 MB (56.41%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -4,11 +4,8 @@ class Solution:
          
         dp = [[0]*2 for _ in range(n+2)]
 
-        for i in range(n-1,-1,-1):
-            for j in range(2):
-                if j==1:
-                    dp[i][j] = max(-p[i] + dp[i+1][0],dp[i+1][1])
-                else:
-                    dp[i][j] = max(p[i] + dp[i+2][1], dp[i+1][0])
+        for i in range(n-1,-1,-1):                            
+            dp[i][1] = max(-p[i] + dp[i+1][0],dp[i+1][1])                
+            dp[i][0] = max(p[i] + dp[i+2][1], dp[i+1][0])
         
         return dp[0][1]
