b8066baf3e362d19f2542669effb7e7edef011b4
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-11-05 12:41:02 +0530

    Time: 1 ms (84.59%), Space: 16.8 MB (96.15%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,7 +1,7 @@
 class Solution:
     def maxProfit(self, p: List[int]) -> int:
         n = len(p)
-         
+        ''' 
         dp = [[0]*2 for _ in range(n+2)]
 
         for i in range(n-1,-1,-1):                            
@@ -9,3 +9,17 @@ class Solution:
             dp[i][0] = max(p[i] + dp[i+2][1], dp[i+1][0])
         
         return dp[0][1]
+
+        '''
+
+        front2 = [0]*2
+        front1 = [0]*2
+        cur = [0]*2
+
+        for i in range(n-1,-1,-1):                            
+            cur[1] = max(-p[i] + front1[0],front1[1])                
+            cur[0] = max(p[i] + front2[1], front1[0])
+            front2 = front1[:]
+            front1 = cur[:]
+        
+        return cur[1]
