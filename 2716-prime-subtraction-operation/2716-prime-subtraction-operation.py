3638bfe1386cb5695e79e97fcff805202a995899
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-11-11 14:20:39 +0530

    Time: 23 ms (76.54%), Space: 16.9 MB (35.5%) - LeetHub

---
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.py
@@ -0,0 +1,30 @@
+class Solution:
+    def primeSubOperation(self, nums: List[int]) -> bool:
+        # sieve of eratosthenes
+        isPrime = [True]*1001
+        isPrime[0]=isPrime[1]=False
+        for i in range (2,int(sqrt(1000)+1)):
+            if isPrime[i]:
+                for j in range(i*i,1001,i):
+                    isPrime[j]=False
+                
+        # primes=[i for i in range(2,1001) where isPrime[i]]
+
+        n = len(nums)
+        
+        prev = 0
+        for n in nums:
+            upperBound = n - prev # non-inclusive
+            LargestPrime = 0
+            for i in reversed(range (2,upperBound)):
+                if isPrime[i]:
+                    LargestPrime = i
+                    break
+            if n-LargestPrime <= prev:
+                return False
+                
+            prev = n - LargestPrime
+
+        return True
+
+
