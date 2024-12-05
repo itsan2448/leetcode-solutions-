83706915c1ede42b1d81df5f4f929e01ed6b5adf
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-12-06 00:14:23 +0530

    Time: 3 ms (94.54%), Space: 17.7 MB (5.25%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -1,8 +1,11 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         # memoisation code
-
+        cache = {}
         def dfs(i,j):
+            if (i,j) in cache:
+                return cache[(i,j)]
+
             if i>=len(s) and j>=len(p):
                 return True
             if j>=len(p):
@@ -11,11 +14,14 @@ class Solution:
             match = (i<len(s) and (s[i]==p[j] or p[j]=='.'))
 
             if (j+1)<len(p) and p[j+1]=='*':
-                return ((match and dfs(i+1,j)) or dfs(i,j+2))
+                cache[(i,j)] = ((match and dfs(i+1,j)) or dfs(i,j+2))
+                return cache[(i,j)]
             # use a*
             # not use a*
             if match :
-                return dfs(i+1,j+1)
+                cache[(i,j)] = dfs(i+1,j+1)
+                return cache[(i,j)]
+            cache[(i,j)] = False
             return False
 
         return dfs(0,0)
