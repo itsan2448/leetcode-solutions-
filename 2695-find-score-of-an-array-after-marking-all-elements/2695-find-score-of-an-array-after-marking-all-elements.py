6311529cbee99bcc738f9919660c3b05c2b0395f
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-12-13 14:03:00 +0530

    Time: 483 ms (68.37%), Space: 38.2 MB (75.24%) - LeetHub

---
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findScore(self, nums: List[int]) -> int:
+        score = 0
+        n = len(nums)
+        mark = [False] * n 
+        pq = [(num,i) for i,num in enumerate(nums)]
+        heapify(pq)
+        while pq:
+            smallest,idx = heappop(pq)            
+            if not mark[idx]:
+                # mark[idx]=True
+                if idx-1>=0:
+                    mark[idx-1]=True
+                if idx+1<len(nums):
+                    mark[idx+1]=True
+                score += smallest
+        return score
