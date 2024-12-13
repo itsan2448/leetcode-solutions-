b32d9d1078036d52cfacc93d1cda396e9d71ca2d
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-12-13 14:03:29 +0530

    Time: 515 ms (63.27%), Space: 38.3 MB (72.35%) - LeetHub

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.py
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.py
@@ -7,8 +7,7 @@ class Solution:
         heapify(pq)
         while pq:
             smallest,idx = heappop(pq)            
-            if not mark[idx]:
-                # mark[idx]=True
+            if not mark[idx]:                
                 if idx-1>=0:
                     mark[idx-1]=True
                 if idx+1<len(nums):
