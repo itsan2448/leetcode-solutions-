76cde6a43e671b8e040497c2891a7b2e5f309cfe
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-12-26 13:09:48 +0530

    Time: 119 ms (77.3%), Space: 18.2 MB (59.59%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,11 +1,25 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        dp = {} # (idx,cur_sum)->no. of ways
-        def backtrack(idx,sum): 
-            if (idx,sum) in dp:
-                return dp[(idx,sum)]
-            if idx==len(nums):
-                return 1 if sum==target else 0
-            dp[(idx,sum)] = (backtrack(idx+1,sum+nums[idx])+backtrack(idx+1,sum-nums[idx]))
-            return dp[(idx,sum)]
-        return backtrack(0,0)
+        # # memoisation
+        # dp = {} # (idx,cur_sum)->no. of ways
+        # def backtrack(idx,sum): 
+        #     if (idx,sum) in dp:
+        #         return dp[(idx,sum)]
+        #     if idx==len(nums):
+        #         return 1 if sum==target else 0
+        #     dp[(idx,sum)] = (backtrack(idx+1,sum+nums[idx])+backtrack(idx+1,sum-nums[idx]))
+        #     return dp[(idx,sum)]
+        # return backtrack(0,0)
+
+        # O(n*sum(nums))
+
+        # bottomUp
+        n=len(nums)
+        dp = [defaultdict(int) for i in range(n+1)]
+        dp[0][0]=1 # (0 element, 0 sum)-> 1 way to achieve 
+        for i in range(n):
+            for sum,count in dp[i].items():
+                dp[i+1][sum + nums[i]] += count
+                dp[i+1][sum - nums[i]] += count
+        return dp[len(nums)][target]
+
