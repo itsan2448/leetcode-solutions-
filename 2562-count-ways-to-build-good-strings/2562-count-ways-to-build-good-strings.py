b90935f669cd86a48ea974cda34505ac27276042
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-12-30 17:31:41 +0530

    Time: 189 ms (42.72%), Space: 79.4 MB (50.16%) - LeetHub

---
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countGoodStrings(self, low: int, high: int, zero: int, one: int) -> int:
+        
+        mod=10**9+7
+        dp={}
+        
+        def dfs(length):
+            if length>high:
+                return 0
+            if length in dp:
+                return dp[length]
+            dp[length] = 1 if length>=low else 0
+            dp[length] += dfs(length+zero)+dfs(length+one)
+            dp[length]%=mod
+            return dp[length]
+        return dfs(0)
