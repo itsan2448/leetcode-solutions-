54fa67c10da22dc63222f7a883de15b83cc55b94
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-12-30 17:37:23 +0530

    Time: 115 ms (67.4%), Space: 33.7 MB (62.5%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.py
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.py
@@ -7,13 +7,13 @@ class Solution:
             dp[i]=(dp.get(i-one,0)+dp.get(i-zero,0))%mod
         return sum([dp[i] for i in range(low,high+1)])%mod
 
-        def dfs(length):
-            if length>high:
-                return 0
-            if length in dp:
-                return dp[length]
-            dp[length] = 1 if length>=low else 0
-            dp[length] += dfs(length+zero)+dfs(length+one)
-            dp[length]%=mod
-            return dp[length]
-        return dfs(0)
+        # def dfs(length):
+        #     if length>high:
+        #         return 0
+        #     if length in dp:
+        #         return dp[length]
+        #     dp[length] = 1 if length>=low else 0
+        #     dp[length] += dfs(length+zero)+dfs(length+one)
+        #     dp[length]%=mod
+        #     return dp[length]
+        # return dfs(0)
