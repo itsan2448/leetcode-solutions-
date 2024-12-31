23a436808e734052a7be2421f185c60b1b7b5486
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2024-12-31 12:56:02 +0530

    Time: 4 ms (59.59%), Space: 17.7 MB (28.23%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.py
@@ -8,7 +8,7 @@ class Solution:
             else:
                 dp[i] = min(
                     dp[i - 1] + costs[0],
-                    dp[i - 7] + costs[1] if i - 7 >= 0 else costs[1], 
+                    dp[i-7] + costs[1] if i - 7 >= 0 else costs[1],
                     dp[i - 30] + costs[2] if i - 30 >= 0 else costs[2]
                 )
         return dp[n]
