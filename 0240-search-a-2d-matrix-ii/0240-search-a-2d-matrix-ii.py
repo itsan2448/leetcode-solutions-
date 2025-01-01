9583cbcf7902770757f2b32eed44cdbab20d2656
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2025-01-01 20:47:46 +0530

    Time: 144 ms (46.45%), Space: 24.1 MB (14.13%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchMatrix(self, x: List[List[int]], t: int) -> bool:
+        m,n=len(x),len(x[0])
+        r=0
+        while r<m and n>0:
+            c=x[r][n-1]
+            if c==t:
+                return True
+            if c>t:
+                # ignore last clm
+                n-=1
+            if c<t:
+                # eliminate the entire 1st row
+                r+=1
+        return False
