390e463ffdb1d604c7883dc21c3bce29e66d9d81
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2025-01-02 11:51:06 +0530

    Time: 715 ms (15.64%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,5 +1,11 @@
 # Write your MySQL query statement below
-select distinct Email 
-from Person 
-group by email
-having count(*)>1
+-- select distinct Email 
+-- from Person 
+-- group by email
+-- having count(*)>1
+
+with tmp as(
+    select email,row_number() over(partition by email) as rnk
+    from person
+)
+select distinct email from tmp where rnk>1
