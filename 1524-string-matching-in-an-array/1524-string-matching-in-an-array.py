c3e53ed67c51db6fb96d599625747eb4bc674b61
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2025-01-07 12:48:58 +0530

    Time: 6 ms (14.27%), Space: 18 MB (6.61%) - LeetHub

---
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        ans=[]
+        n=len(words)
+        for i in range(n):
+            for j in range(n):
+                if i!=j and words[j].find(words[i])!=-1:
+                    ans.append(words[i])
+                    break
+        return ans
