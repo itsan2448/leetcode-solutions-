373a7946cec22be0db49be5adb36f84d83968273
ANANYA ROY <108606010+itsan2448@users.noreply.github.com>
2025-02-01 11:40:00 +0530

    Time: 4 ms (7.28%), Space: 17.8 MB (22.9%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.py
+++ 3429-special-array-i/3429-special-array-i.py
@@ -1,9 +1,11 @@
 class Solution:
     def isArraySpecial(self, nums: List[int]) -> bool:
-        n=len(nums)
-        if n==1:
-            return True
-        for i in range(1,n):
-            if (nums[i-1]+nums[i])%2==0:
-                return False
-        return True
+        # n=len(nums)
+        # if n==1:
+        #     return True
+        # for i in range(1,n):
+        #     if (nums[i-1]+nums[i])%2==0:
+        #         return False
+        # return True
+
+        return all((x^y)&1 for x,y in zip(nums,nums[1:]))
